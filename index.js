c36467d70508cd1bb37052bbbfbfea452d832c63
marifsulaksono <marifsulaksono@gmail.com>
2023-11-22 13:03:06 +0700

    feat:create response

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./connection')
+const response = require('./response')
 const app = express()
 const port = 8000
 
-app.get("/", (request, response) => {
-    response.send('Hello world')
+app.get("/", (req, res) => {
+    const query = "select * from transaction"
+    db.query(query, (error, result) => {
+        response(200, result, "success get all transaction", res)
+    })
 })
 
 app.listen(port, () => {
