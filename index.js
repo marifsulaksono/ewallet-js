3584460581cceebe0f6e25fb69a365ea152ade8b
marifsulaksono <marifsulaksono@gmail.com>
2023-11-22 13:30:08 +0700

    feat:get transaction by id

--- index.js
+++ index.js
@@ -5,7 +5,35 @@ const response = require('./response')
 const app = express()
 const port = 8000
 
-app.get("/", (req, res) => {
+app.get("/transactions", (req, res) => {
+    const query = "select * from transaction"
+    db.query(query, (error, result) => {
+        response(200, result, "success get all transaction", res)
+    })
+})
+
+app.get("/transactions/:id", (req, res) => {
+    const query = `select * from transaction where id = ${req.params.id}`
+    db.query(query, (error, result) => {
+        response(200, result, `success get transaction ${req.params.id}`, res)
+    })
+})
+
+app.post("/transactions", (req, res) => {
+    const query = "select * from transaction"
+    db.query(query, (error, result) => {
+        response(200, result, "success get all transaction", res)
+    })
+})
+
+app.put("/transactions", (req, res) => {
+    const query = "select * from transaction"
+    db.query(query, (error, result) => {
+        response(200, result, "success get all transaction", res)
+    })
+})
+
+app.delete("/transactions", (req, res) => {
     const query = "select * from transaction"
     db.query(query, (error, result) => {
         response(200, result, "success get all transaction", res)
